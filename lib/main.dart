cf285faf41c0b003badeee99dc867f10d1b14abe
Sunitcha64 <143933752+Sunitcha64@users.noreply.github.com>
2023-09-03 16:35:43 +0700

    Demo

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'Screen/Slide Menu.dart';
+import 'Screen/home.dart'; 
+
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'User CRUD',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Home(),
+        '/login': (context) => Login(),
+      },
+    );
+  }
+}
